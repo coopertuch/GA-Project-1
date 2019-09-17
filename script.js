a74a6d951d0fa459ed7bf35d51a8a6f95392f587
Cooper Tuch <coopertuch@gmail.com>
2019-09-17 09:38:17 -0700

    added variables and event listeners, building out basic functions, edits to html css

--- script.js
+++ script.js
@@ -8,26 +8,93 @@
 
 // VARIABLES & OBJECTS //
 // Have Fun Questions Object
+haveFunQuestions = [
+    'sample fun question 1',
+    'sample fun question 2',
+    'sample fun question 3',
+    'sample fun question 4',
+    'sample fun question 5',
+];
 // Get Deep Questions Object
+getDeepQuestions = [
+    'sample deep question 1',
+    'sample deep question 2',
+    'sample deep question 3',
+    'sample deep question 4',
+    'sample deep question 5',
+];
 // This Or That Questions Object
+thisOrThatQuestions = [
+    'sample this or that question 1',
+    'sample this or that question 2',
+    'sample this or that question 3',
+    'sample this or that question 4',
+    'sample this or that question 5',
+];
 // Top 3 Questions Object
+topQuestions = [
+    'sample top question 1',
+    'sample top question 2',
+    'sample top question 3',
+    'sample top question 4',
+    'sample top question 5',
+];
 // Custom Questions Object (Empty Array)
+customQuestions = [
+    'sample custom question 1',
+    'sample custom question 2',
+    'sample custom question 3',
+    'sample custom question 4',
+    'sample custom question 5',
+];
 // + Input Object (Push to Array)
 
-// EVENT LISTENERS //
+// DOM & EVENT LISTENERS //
 // Have Fun Button
-// Get Deep Questions Button
-// This Or That Questions Button
-// Top 3 Questions Button
-// Custom Questions Button 
-// + Input Button 
+// const haveFun = document.querySelector('.fun');
+// haveFun.addEventListener('click', );
 
-// Timer Button
+// Get Deep Questions Button
+// const getDeep = document.querySelector('.deep');
+// getDeep.addEventListener('click', );
+
+// This Or That Questions Button
+// const thisOrThat = document.querySelector('.thisorthat');
+// thisOrThat.addEventListener('click', );
+
+// Top 3 Questions Button
+// const top = document.querySelector('.top');
+// top.addEventListener('click', );
+
+// Custom Questions Button 
+// const custom = document.querySelector('.custom');
+// custom.addEventListener('click', );
+
+// + Input Button 
+// const add = document.querySelector('.add');
+// add.addEventListener('click', );
+
 // Thumbs Up Button
+// const thumbsUp = document.querySelector('.thumbsup');
+// thumbsUp.addEventListener('click', );
+
 // Thumbs Down Button
+// const thumbsDown = document.querySelector('.thumbsdown');
+// thumbsDown.addEventListener('click', );
+
 // Arrow Left Button
+// const previous = document.querySelector('.previous');
+// previous.addEventListener('click', );
+
 // Arrow Right Button
-// Logo (Resets Page)
+// const next = document.querySelector('.next');
+// next.addEventListener('click', );
+
+// Reset (Resets Page)
+const reset = document.querySelector('.reset');
+reset.addEventListener('click', function() {
+    location.reload();
+});
 
 // FUNCTIONS //
 // Have Fun Button Randomly Sets Questions 
@@ -42,4 +109,7 @@
 // Thumbs Down Button (Reports Negative Feedback)
 // Arrow Left Button Slides Back
 // Arrow Right Button Slides Forward
-// Logo (Resets Page)
+
+// SOURCES //
+// https://www.w3schools.com/jsref/met_loc_reload.asp
+// page reset
