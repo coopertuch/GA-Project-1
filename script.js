983b84fa62c7ee95e2cb5737571e04edd64b5c6f
Cooper Tuch <coopertuch@gmail.com>
2019-09-17 16:11:33 -0700

    edits

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 // COOP'S QUESTIONS GAME FLOW - Good questions skim the surface, Great questions drive meaningful connection. 
 
 // ACTION ITEMS //
-// Set Up Input + Submit, Push to Custom Questions Array
+// Set Up Input + Submit, Push to Custom Questions Array. (push input.value to custom array)
 // Each Iteration Pops Current Question
 // Thumbs Up (Reports Positive), Thumbs Down (Reports Negative), Pops
 // If (array == 0), alert('You've answered all the questions in this category! Try another category, add your own questions, or reset.');
+// CSS Button Change Color When Clicked
+// Add Questions
 // Set up GitHub Website
 
 // VARIABLES & OBJECTS //
 haveFunQuestions = [
-    'sample fun question 1',
-    'sample fun question 2',
-    'sample fun question 3',
-    'sample fun question 4',
-    'sample fun question 5',
+    'Sample fun question 1',
+    'Sample fun question 2',
+    'Sample fun question 3',
+    'Sample fun question 4',
+    'Sample fun question 5',
 ];
 getDeepQuestions = [
-    'sample deep question 1',
-    'sample deep question 2',
-    'sample deep question 3',
-    'sample deep question 4',
-    'sample deep question 5',
+    'Sample deep question 1',
+    'Sample deep question 2',
+    'Sample deep question 3',
+    'Sample deep question 4',
+    'Sample deep question 5',
 ];
 thisOrThatQuestions = [
-    'sample this or that question 1',
-    'sample this or that question 2',
-    'sample this or that question 3',
-    'sample this or that question 4',
-    'sample this or that question 5',
+    'Sample this or that question 1',
+    'Sample this or that question 2',
+    'Sample this or that question 3',
+    'Sample this or that question 4',
+    'Sample this or that question 5',
 ];
 topQuestions = [
-    'sample top question 1',
-    'sample top question 2',
-    'sample top question 3',
-    'sample top question 4',
-    'sample top question 5',
+    'Sample top question 1',
+    'Sample top question 2',
+    'Sample top question 3',
+    'Sample top question 4',
+    'Sample top question 5',
 ];
 customQuestions = [];
 
@@ -81,7 +83,7 @@ function startThisGame() {
 function startTopGame() {
     for (i = 0; i < topQuestions.length; i++) {
     topRandom = topQuestions[Math.floor(Math.random()*topQuestions.length)];
-    question.innerHTML = topRandom;
+    question.innerHTML = (`Rank your Top 3: ${topRandom}!`);
     }
 }
 
@@ -104,8 +106,6 @@ reset.addEventListener('click', function() {
 // Thumbs Down Button (Reports Negative Feedback) (Pops Item)
 // const thumbsDown = document.querySelector('.thumbsdown');
 // thumbsDown.addEventListener('click', );
-
-// Timer
 
 // SOURCES //
 // https://www.w3schools.com/jsref/met_loc_reload.asp
