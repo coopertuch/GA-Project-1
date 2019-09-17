02f22058e51935debd100d64657efef39b9f8ca9
Cooper Tuch <coopertuch@gmail.com>
2019-09-16 22:19:24 -0700

    html css files set up, pseudo code started

---
+++ script.js
@@ -0,0 +1,45 @@
+// COOP'S QUESTIONS GAME FLOW - Good questions skim the surface, Great questions drive meaningful connection. 
+// Multiple categories - Fun, Romantic, Deep, Custom, +
+// Randomly draws questions from selected category.
+// User can slideshow through questions.
+// Tracks a "thumbs up" or "thumbs down", removing the question.
+// Allow users to add their own questions in the + section.
+// Custom section contains all questions added from +
+
+// VARIABLES & OBJECTS //
+// Have Fun Questions Object
+// Get Deep Questions Object
+// This Or That Questions Object
+// Top 3 Questions Object
+// Custom Questions Object (Empty Array)
+// + Input Object (Push to Array)
+
+// EVENT LISTENERS //
+// Have Fun Button
+// Get Deep Questions Button
+// This Or That Questions Button
+// Top 3 Questions Button
+// Custom Questions Button 
+// + Input Button 
+
+// Timer Button
+// Thumbs Up Button
+// Thumbs Down Button
+// Arrow Left Button
+// Arrow Right Button
+// Logo (Resets Page)
+
+// FUNCTIONS //
+// Have Fun Button Randomly Sets Questions 
+// Get Deep Questions Button Randomly Sets Questions 
+// This Or That Questions Button Randomly Sets Questions 
+// Top 3 Questions Button Randomly Sets Questions 
+// Custom Questions Button Randomly Sets Questions From Custom Array
+// + Input Button Prompts User To Input Questions
+
+// Timer Button Starts Times (60 Seconds)
+// Thumbs Up Button (Reports Positive Feedback)
+// Thumbs Down Button (Reports Negative Feedback)
+// Arrow Left Button Slides Back
+// Arrow Right Button Slides Forward
+// Logo (Resets Page)
